a87c54a71bf32b3bab917d22a30e08494a4a85de
ekwunifeifenna <ifennaekwunife73@gmail.com>
2023-11-10 12:11:29 -0500

    Finish setting up the react environment Install dependencies needed

--- fitness-website/src/App.jsx
+++ fitness-website/src/App.jsx
@@ -1,19 +1,29 @@
 import { useState } from 'react'
 import {Route, Routes} from "react-router-dom"
 import {Box} from "@mui/material"
+
+
 import './App.css'
+import ExerciseDetail from "./pages/ExerciseDetail"
+import Home from "./pages/Home"
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
 // the box is a div with some shading and some colours
   return (
-    <Box width="400px">
+
+    // Make sure the website is responsive on larger devices with the sx property and margin auto
+    <Box width="400px" sx={{ width: {xl: '1488px'}}} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/exercise/:id" element={<ExercideDetail />}/>
-
+        <Route path="/exercise/:id" element={<ExerciseDetail />}/>
       </Routes>
+      <Footer />
+
 
 
  
