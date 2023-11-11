6bb67b80a6845313e11c345da5914004950dc813
ekwunifeifenna <ifennaekwunife73@gmail.com>
2023-11-10 21:47:33 -0500

    Set up the Next js enviroment

--- fitness-website/src/App.jsx
+++ fitness-website/src/App.jsx
@@ -1,13 +1,15 @@
+import React from "react"
 import { useState } from 'react'
-import {Route, Routes} from "react-router-dom"
-import {Box} from "@mui/material"
+import {Route, Routes} from 'react-router-dom'
+import {Box} from '@mui/material'
+
 
 
 import './App.css'
-import ExerciseDetail from "./pages/ExerciseDetail"
-import Home from "./pages/Home"
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import ExerciseDetail from './pages/ExerciseDetail.jsx'
+import Home from './pages/Home.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 
 function App() {
